6442e553354759c63690c707f393122fec0054e0
Matthew Curry <matt.curry56@gmail.com>
2023-09-09 21:08:33 -0400

    some base components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
