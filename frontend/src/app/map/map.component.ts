7311d38e6055129bc4139a97dacef6ae6f8f04de
Matthew Curry <matt.curry56@gmail.com>
2023-09-10 17:20:09 -0400

    a map renders

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'
+
+const TILE_LAYER_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const OPEN_STREET_ATTRIBUTION = '&copy; <a href="http://www.openstreetmap.org/copyright>OpenStreetMap></a>"';
+const MAX_ZOOM = 18;
+const MIN_ZOOM = 6;
+const CENTER_LATTITUDE = 39.8355;
+const CENTER_LONGITUDE = -99.0909;
+const MAP_ZOOM = 1;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+
+  private map: L.Map;
+  private centroid: L.LatLngExpression = [CENTER_LATTITUDE, CENTER_LONGITUDE];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+    // create map
+    this.map = L.map('map', {
+      center: this.centroid,
+      zoom: MAP_ZOOM
+    })
+
+    // add tiles
+    const tiles = L.tileLayer(TILE_LAYER_URL, {
+      maxZoom: MAX_ZOOM,
+      minZoom: MIN_ZOOM,
+      attribution: OPEN_STREET_ATTRIBUTION
+    })  
+
+    tiles.addTo(this.map)
+
+  }
 
 }
